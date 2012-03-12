5995353239b2982e77870da319d67699c9252d49
Creep Thor <creeptor@gmail.com>
2012-03-12 22:30:27 +0200

    Example

---
+++ example.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Simple script for backup MySQL database on remote server
+ */
+
+include("ssh.class.php");
+
+$server_host = "ssh.example.com";
+$server_login = "root";
+$server_password = "123456"; // Didn't user simple passwords
+
+$mysql_user = "root";
+$mysql_password = "123456"; // Didn't user simple passwords
+$mysql_database = "base";
+
+// Connect:
+$ssh = new ssh($server_host, $server_login, $server_password);
+
+// Make backup:
+$ssh("mysqldump -u $mysql_user -p{$mysql_password} $mysql_database > /tmp/bases", "gzip -9 /tmp/bases");
+
+// Download backup:
+$ssh->download("/tmp/bases.gz", "/tmp/bases.gz");
+
+?>
